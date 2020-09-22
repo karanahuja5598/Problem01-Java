e7bee73222b4cbb50f1c6911b707aeb402dc2d8f
Karan Ahuja <karanahuja5598@gmail.com>
2020-09-22 17:42:06 -0500

    Finished everything

--- src/main/java/cd/get/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/cd/get/ready/algorithms/WuTangForeverBuzz.java
@@ -3,6 +3,19 @@ package cd.get.ready.algorithms;
 public class WuTangForeverBuzz {
 
     public String wuTangClan(int value){
-        return null;
+        String result = "";
+        for(int i = 1; i <= value; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                result += "WuTangForever\n";
+            } else if(i % 3 == 0) {
+                result += "Wu\n";
+            } else if(i % 5 == 0) {
+                result += "Tang\n";
+            } else {
+                result += i + "\n";
+            }
+        }
+
+        return result;
     }
 }
--- src/test/java/cd/get/ready/algorithms/WuTangForeverBuzzTest.java
+++ src/test/java/cd/get/ready/algorithms/WuTangForeverBuzzTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class WuTangForeverBuzzTest {
 
+    @Test
     public void wuTangClanTest(){
         WuTangForeverBuzz wuTangForeverBuzz = new WuTangForeverBuzz();
 
@@ -14,6 +15,7 @@ public class WuTangForeverBuzzTest {
         Assert.assertEquals(expected,actual);
     }
 
+    @Test
     public void wuTangClanTest2(){
         WuTangForeverBuzz wuTangForeverBuzz = new WuTangForeverBuzz();
         String expected = "1\n2\nWu\n4\nTang\nWu\n7\n8\nWu\nTang\n11\nWu\n13\n14\nWuTangForever\n16\n17\nWu\n19\nTang\nWu\n22\n"+
